636c61ff54b82574439275717266b091f898c4a7
Dhruv Kaliraman <112497058+dhruvkaliraman7@users.noreply.github.com>
2024-02-27 18:43:16 -0500

    Time: 281 ms (55.76%), Space: 18.5 MB (66.20%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -1,17 +1,19 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        grid=defaultdict(list)
+        maxRank = 0
+        adj = defaultdict(set)
+        # Construct adjency list 'adj', where adj[node] stores all nodes connected to 'node'.
         for road in roads:
-            grid[road[0]].append(road[1])
-            grid[road[1]].append(road[0])
-        maxn=0
-        for key1 in grid:
-            for key2 in grid:
-                if key1==key2:
-                    continue
-                if key1 in grid[key2]:
-                    maxn= max(maxn, len(grid[key1])+len(grid[key2])-1)
-                else:
-                    maxn= max(maxn, len(grid[key1])+len(grid[key2]))
-        return maxn
-            
+            adj[road[0]].add(road[1])
+            adj[road[1]].add(road[0])
+
+        # Iterate on each possible pair of nodes.
+        for node1 in range(n):
+            for node2 in range(node1 + 1, n):
+                currentRank = len(adj[node1]) + len(adj[node2])
+                if node2 in adj[node1]:
+                    currentRank -= 1
+                # Find the current pair's respective network rank and store if it's maximum till now.
+                maxRank = max(maxRank, currentRank)
+        # Return the maximum network rank.
+        return maxRank
