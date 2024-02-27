e2b0a5a10005279b9b1bcf4c1e1fbdd86c2bea95
Dhruv Kaliraman <112497058+dhruvkaliraman7@users.noreply.github.com>
2024-02-27 18:54:34 -0500

    Time: 34 ms (79.69%), Space: 16.5 MB (94.01%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,19 +1,7 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         dic=defaultdict(int)
-        tmp=['b','a','l','o','n']
-        count=0
-        for ch in text:
-            dic[ch]+=1
-        minn=float('inf')
-        count=0
-        for key in dic:
-            if key in tmp:
-                if key in ['l','o']:
-                    minn=min(minn,dic[key]//2)
-                else:
-                    minn=min(minn,dic[key])
-                count+=1
-        if count!=5 or minn==float('inf'):
-            return 0
-        return minn 
+        for t in text:
+            dic[t]+=1
+        # print(dic)
+        return min(dic['b'],dic['a'],dic['l']//2,dic['o']//2,dic['n'])
