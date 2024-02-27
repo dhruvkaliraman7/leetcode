b79c5c0f110e3b77a4b886d7e5b9457218a77918
Dhruv Kaliraman <112497058+dhruvkaliraman7@users.noreply.github.com>
2024-02-27 03:39:17 -0500

    Time: 39 ms (50.52%), Space: 16.7 MB (60.16%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,44 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = ''
+        a_r , b_r = len(a) - 1 , len(b) - 1
+        carry = 0
+        while a_r >= 0 and b_r >= 0:
+            tmp = carry + int(a[a_r]) + int(b[b_r])
+            if tmp == 0:
+                res += '0'
+                carry = 0
+            elif tmp == 1:
+                res += '1'
+                carry = 0
+            elif tmp == 2:
+                res += '0'
+                carry = 1
+            else:
+                res += '1'
+                carry = 1
+            a_r -= 1
+            b_r -= 1
+        if a_r >= 0:
+            rem_str = a[:a_r + 1]
+        else:
+            rem_str = b[:b_r + 1]
+        for i in range(len(rem_str)-1,-1,-1):
+            tmp = carry + int(rem_str[i])
+            if tmp == 0:
+                res += '0'
+                carry = 0
+            elif tmp == 1:
+                res += '1'
+                carry = 0
+            elif tmp == 2:
+                res += '0'
+                carry = 1
+        if carry == 1:
+            res  += '1'
+        return res[::-1]
+            
+        
+        
+            
+                
