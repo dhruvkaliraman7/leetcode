1b2c236e5848012dc4ec66501e8ed041171105d9
Dhruv Kaliraman <112497058+dhruvkaliraman7@users.noreply.github.com>
2024-02-28 00:37:07 -0500

    Time: 98 ms (30.22%), Space: 18.5 MB (52.82%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,33 +1,24 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        indegree=defaultdict(int)
-        neighs=defaultdict(list)
+        indegree=[0]*numCourses
+        dic=defaultdict(list)
         for course,prereq in prerequisites:
+            dic[prereq].append(course)
             indegree[course]+=1
-            indegree[prereq]+=0
-            neighs[prereq].append(course)
-        
-        q=collections.deque()
+            
+        tmp=collections.deque([])
+        for i in range(len(indegree)):
+            if indegree[i]==0:
+                tmp.append(i)
         res=[]
-        for i in range(numCourses):
-            if i not in indegree:
-                res.append(i)
-        
-        for key in indegree:
-            if indegree[key]==0:
-                q.append(key)
-                res.append(key)   
-        # print(q)
-        while q:
-            tmp=q.popleft()
-            for neigh in neighs[tmp]:
-                indegree[neigh]-=1
-                if indegree[neigh]==0:
-                    q.append(neigh)
-                    res.append(neigh)
-        # print(indegree)
-        # print(q)
-        for key in indegree:
-            if indegree[key]>0:
+        while tmp:
+            tmp_node=tmp.popleft()
+            res.append(tmp_node)
+            for course in dic[tmp_node]:
+                indegree[course]-=1
+                if indegree[course]==0:
+                    tmp.append(course)
+        for i in range(len(indegree)):
+            if indegree[i]!=0:
                 return []
         return res
