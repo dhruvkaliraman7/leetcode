6b95087abbd354e2d11d590aad443610103b6979
Dhruv Kaliraman <112497058+dhruvkaliraman7@users.noreply.github.com>
2024-03-03 15:49:10 -0500

    Time: 474 ms (49.26%), Space: 19.8 MB (60.74%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,7 +2,7 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         r , c = len(mat) , len(mat[0])
         dirs = [(1,0) , (0,1) , (-1,0) , (0,-1)]
-        new_grid = [[-1 for _ in range(c)] for _ in range(r)]
+        #new_grid = [[-1 for _ in range(c)] for _ in range(r)]
         q=collections.deque()
         visit= set()
         for i in range(r):
@@ -14,7 +14,7 @@ class Solution:
         while q:
             for i in range(len(q)):
                 tmp_loc = q.popleft()
-                new_grid[tmp_loc[0]][tmp_loc[1]] = count
+                mat[tmp_loc[0]][tmp_loc[1]] = count
                 for new_r,new_c in dirs:
                     comp_x, comp_y = new_r+tmp_loc[0] , new_c +tmp_loc[1]
                     if comp_x <0 or comp_y < 0 or comp_x >=r or comp_y >= c or (comp_x,comp_y) in visit:
@@ -22,7 +22,7 @@ class Solution:
                     q.append((comp_x,comp_y))
                     visit.add((comp_x,comp_y))
             count+=1
-        return new_grid
+        return mat
                     
                 
             
