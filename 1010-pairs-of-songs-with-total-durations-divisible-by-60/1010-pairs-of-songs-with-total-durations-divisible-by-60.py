89dafc15ab912947583a3a451d395a11c6a9a2e8
Dhruv Kaliraman <112497058+dhruvkaliraman7@users.noreply.github.com>
2024-03-04 00:08:13 -0500

    Time: 234 ms (37.33%), Space: 20.8 MB (59.51%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -9,11 +9,11 @@ class Solution:
                 res += dic[60 - t%60]
             dic[t%60] += 1
         return res
-        res=0
-        # print(dic)
-        for k,v in dic.items(): 
-            if k==0 or k==30:
-                res += (v*(v-1)/2)
-            elif (60-k) in dic:
-                res += (v*dic[60-k])/2
-        return int(res)
+        # res=0
+        # # print(dic)
+        # for k,v in dic.items(): 
+        #     if k==0 or k==30:
+        #         res += (v*(v-1)/2)
+        #     elif (60-k) in dic:
+        #         res += (v*dic[60-k])/2
+        # return int(res)
