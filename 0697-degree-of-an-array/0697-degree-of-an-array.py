75315b5ed302b4cc5eb45c9b558a9b39da4e4848
Dhruv Kaliraman <112497058+dhruvkaliraman7@users.noreply.github.com>
2024-03-04 00:37:15 -0500

    Time: 212 ms (22.88%), Space: 18.2 MB (52.22%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -10,7 +10,7 @@ class Solution:
                 dic[num] = [tmp[0]+1, tmp[1], i]
             maxn = max(maxn, dic[num][0])
         num = float('inf')
-        print(dic)
+        # print(dic)
         for k in dic:
             if dic[k][0] == maxn:
                 num = min(num, dic[k][2]- dic[k][1])
