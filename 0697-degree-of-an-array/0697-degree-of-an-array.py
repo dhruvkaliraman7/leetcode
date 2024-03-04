bad1d355a9443efee1f5c4fc8c757fd9c9a8362d
Dhruv Kaliraman <112497058+dhruvkaliraman7@users.noreply.github.com>
2024-03-04 00:37:05 -0500

    Time: 214 ms (22.60%), Space: 18.1 MB (57.59%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        maxn = 0
+        dic = {}
+        for i,num in enumerate(nums):
+            if num not in dic:
+                dic[num] = [1,i,i]
+            else:
+                tmp = dic[num]
+                dic[num] = [tmp[0]+1, tmp[1], i]
+            maxn = max(maxn, dic[num][0])
+        num = float('inf')
+        print(dic)
+        for k in dic:
+            if dic[k][0] == maxn:
+                num = min(num, dic[k][2]- dic[k][1])
+        return num + 1
