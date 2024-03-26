75e80efda4cb4c02208c639c3ff70806fc8ed84c
Dhruv Kaliraman <112497058+dhruvkaliraman7@users.noreply.github.com>
2024-03-26 02:12:30 -0400

    Time: 9195 ms (5.01%), Space: 18.8 MB (58.21%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,36 +1,33 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        self.trie={}
-        def insert(word):
-            tmp=self.trie
-            for ch in word:
-                if ch not in tmp:
-                    tmp[ch]={}
-                tmp=tmp[ch]
-            tmp['*']=''
-        
+        self.trie = {}
+        self.res = set()
+        def addWord(word,mapTrie):
+            for w in word:
+                if w not in mapTrie:
+                    mapTrie[w]={}
+                mapTrie = mapTrie[w]    
+            mapTrie['#']='#'
+            return
         for word in words:
-            insert(word)
-            
-        res=set()
-        r,c=len(board),len(board[0])
-        dirs=[(0,1),(1,0),(-1,0),(0,-1)]
-        def dfs(i,j,dic,tmp_str,visit):
-            if i<0 or j<0 or i>=r or j>=c or (i,j) in visit or board[i][j] not in dic:
-                return 
-            
-            if '*' in dic[board[i][j]]:
-                res.add(tmp_str+board[i][j])
-                
-            
-            visit.add((i,j))
-            for new_r,new_c in dirs:
-                dfs(i+new_r, j+new_c,dic[board[i][j]],tmp_str+board[i][j],visit)
-            visit.remove((i,j))
+            addWord(word,self.trie)
+        r,c =len(board), len(board[0])
+        
+        dirs=[(1,0),(0,1),(-1,0),(0,-1)]
+        
+        def dfs(i,j,visit,trie,curWord):
+            if '#' in trie:
+                self.res.add(curWord)
+            if 0<=i<r and 0<=j<c and (i,j) not in visit and board[i][j] in trie:
+                visit.add((i,j))
+                # curWord += grid[i][j] 
+                for newX,newY in dirs:
+                    dfs(i+newX,j+newY,visit,trie[board[i][j]],curWord+board[i][j] )
+                visit.remove((i,j))
+                    
         for i in range(r):
             for j in range(c):
-                dfs(i,j,self.trie,'',set())
-        return list(res)
-            
-        
-            
+                if board[i][j] in self.trie:
+                    dfs(i,j,set(),self.trie,'')
+        return list(self.res)
+                    
